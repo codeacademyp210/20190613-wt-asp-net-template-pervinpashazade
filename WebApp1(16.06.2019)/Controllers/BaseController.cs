3355e8e1e819847fce205c39eda87bb7994265a8
pervinpashazade <ParvinKHp@code.local>
2019-06-18 22:45:52 +0400

    sql

---
+++ WebApp1(16.06.2019)/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp1_16._06._2019_.Models;
+
+namespace WebApp1_16._06._2019_.Controllers
+{
+    public class BaseController : Controller
+    {
+        BemaxEntities db = new BemaxEntities();
+
+        Setting Setting;
+
+        public BaseController()
+        {
+            Setting = db.Settings.FirstOrDefault();
+        }
+    }
+}
