7395e3b87e23be3227188081b00ed00e32fb5879
pervinpashazade <ParvinKHp@code.local>
2019-06-18 22:06:28 +0400

    front backend

---
+++ WebApp1(16.06.2019)/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp1_16._06._2019_.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp1(16.06.2019)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApp1_16._06._2019_.MvcApplication" Language="C#" %>
